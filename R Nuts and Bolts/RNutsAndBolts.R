779aed841d6c757b80db395e61ed75070ac4fd88
debbieorantoy <debbieorantoy@gmail.com>
2018-07-08 22:50:02 +0800

    Activity2

--- R Nuts and Bolts/RNutsAndBolts.R
+++ R Nuts and Bolts/RNutsAndBolts.R
@@ -42,3 +42,114 @@ unclass(y)
 
 y<-factor(c("male", "female", "female", "male", "male"),levels = c("male", "female"))
 y
+
+x<- c(1,2,NA,10,3)
+is.na(x)
+is.nan(x)
+x<-c(1,2,NaN,NA,4)
+is.na(x)
+is.nan(x)
+
+#Data Frames
+x<-data.frame(foo=1:4,bar=c(T,T,F,F))
+x
+nrow(x)
+ncol(x)
+#Names
+x<-1:3
+names(x)
+names(x)<-c("foo","bar","norf")
+x
+names(x)
+#Names-List
+x<-list(a = 1,b = 2,c = 3)
+x
+#Matrix Names
+m<-matrix (1:4,nrow = 2, ncol = 2 )
+dimnames(m)<- list(c("a","b"), c("c", "d"))
+m
+
+#dput-ing
+y<-data.frame(a=1,b="a")
+dput(y)
+dput(y,file="y.R")
+new.y<-dget("y.R")
+new.y
+#Dumping
+x <-"foo"
+y <-data.frame(a=1, b="a")
+dump(c("x","y"),file="data.R")
+rm(x,y)
+source("data.R")
+y
+x
+#Subsetting
+x <- c("a","b","c","c","d","a")
+x[1]
+x[2]
+x[1:4]
+x[x>"a"]
+u <- x>"a"
+u
+x[u]
+#Subsetting Lists
+x<-list(foo=1:4, bar=0.6)
+x[1]
+x[[1]]
+x$bar
+x[["bar"]]
+x["bar"]
+x<- list(foo=1:4,bar=0.6,baz="hello")
+x[c(1,3)]
+x<- list(foo=1:4, bar=0.6, baz="hello")
+name<-"foo"
+x[[name]]
+x$name
+x$foo
+#Subsetting Nested Elements
+x<-list(a=list(10,12,14), b=c(3.14,2.81))
+x[[c(1,3)]]
+x[[1]][[3]]
+x[[c(2,1)]]
+#Subsetting a Matrix
+x<-matrix(1:6,2,3)
+x[1,2]
+x[2,1]
+x[1, ]
+x[ ,2]
+x<-matrix(1:6, 2,3)
+x[1,2]
+x[1,2, drop=FALSE]
+x<matrix(1:6,2,3)
+x[1, ]
+x[1, , drop=FALSE]
+#Partial Matching
+x<-list(aardvark=1:5)
+x$a
+x[["a"]]
+x[["a", exact= FALSE]]
+x<- c(1,2,NA,4,NA,5)
+bad<-is.na(x)
+x[!bad]
+x<-c(1,2,NA,4,NA,5)
+y<-c("a","b",NA, "d", NA, "f")
+good<-complete.cases(x,y)
+good
+x[good]
+y[good]
+#Removing NA values
+airquality[1:6, ]
+good<-complete.cases(airquality)
+airquality[good, ][1:6, ]
+#Vectorized Operations
+x<- 1:4;y<-6:9
+x+y
+x>2
+x>=2
+y==8
+x*y
+x/y
+x<-matrix(1:4,2,2);y<-matrix(rep(10,4),2,2)
+x*y
+x/y
+x %*% y
